f851d7d8e7d247575a4a4138a53f59d1bf51955c
Sushil97 <sushil500530@gmail.com>
2023-12-16 00:00:16 +0600

    add loader from react loader

--- src/pages/home/furni-category/FurniCate.jsx
+++ src/pages/home/furni-category/FurniCate.jsx
@@ -2,6 +2,8 @@ import useFurCategory from "../../../hooks/useFurCategory";
 import AOS from 'aos';
 import FurniCategCard from './FurniCategCard';
 import Container from "../../../shared/container/Container";
+import { IoMdShare } from "react-icons/io";
+import { Discuss, ThreeDots } from "react-loader-spinner";
 const FurniCate = () => {
     const [category, refetch, isLoading] = useFurCategory();
     console.log(category);
@@ -15,7 +17,7 @@ const FurniCate = () => {
     return (
         <Container>
             <h1 className="text-3xl font-bold text-center my-8">Find Your Fevourite Furniture</h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
                 {
                     category?.map(category => <FurniCategCard
                         key={category._id}
@@ -23,6 +25,22 @@ const FurniCate = () => {
                     ></FurniCategCard>)
                 }
             </div>
+            {
+                category?.length === 0 && <>
+                    <div className="w-full h-[60vh] flex item-center justify-center">
+                        <ThreeDots
+                            height="100"
+                            width="100"
+                            radius="9"
+                            color="#4fa94d"
+                            ariaLabel="three-dots-loading"
+                            wrapperStyle={{}}
+                            wrapperClassName=""
+                            visible={true}
+                        />
+                    </div>
+                </>
+            }
         </Container>
     );
 };
