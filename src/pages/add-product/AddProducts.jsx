d27cabab2d677e7faa35a46905cf8ccef08509e2
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-13 18:07:24 +0600

    add change input border from add product

--- src/pages/add-product/AddProducts.jsx
+++ src/pages/add-product/AddProducts.jsx
@@ -8,6 +8,7 @@ import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import useFindMananger from "../../hooks/useFindMananger";
 import Loader from "../../shared/Loader";
+import useFurCategory from "../../hooks/useFurCategory";
 
 
 const AddProducts = () => {
@@ -15,9 +16,12 @@ const AddProducts = () => {
     const [loading, setLoading] = useState(false);
     const axiosSecure = useAxiosSecure();
     const navigate = useNavigate();
-    const [currentManager,,isLoading] = useFindMananger();
+    const [currentManager, , isLoading] = useFindMananger();
+    const [category, ,] = useFurCategory();
+    console.log(category);
 
-    if(isLoading){
+
+    if (isLoading) {
         return <Loader />
     }
     const handleAddedProduct = async (e) => {
@@ -36,7 +40,15 @@ const AddProducts = () => {
         const owner_name = user?.displayName;
         const email = user?.email;
         const location = form.location.value;
+        // {
 
+        //         category,
+        //         thumbnail1,
+        //         thumbnail2,
+        //         rating,
+
+
+        // }
         try {
             const loadImage = await imageUpload(image);
             const addProducts = {
@@ -89,21 +101,21 @@ const AddProducts = () => {
                                     <label htmlFor='location' className='dark:text-white block text-black font-medium'>
                                         Product Name
                                     </label>
-                                    <input className='w-full px-4 py-3 text-gray-800 border rounded-md input border-blue-400 ' name='product_name' id='product_name' type='text' placeholder='Product name' required
+                                    <input className='w-full px-4 py-3 text-gray-800 border rounded-md input input-info ' name='product_name' id='product_name' type='text' placeholder='Product name' required
                                     />
                                 </div>
                                 <div className='space-y-1 w-full'>
                                     <label htmlFor='location' className='block dark:text-white text-black font-medium'>
                                         Location
                                     </label>
-                                    <input className='w-full px-4 py-3 text-gray-800 border rounded-md input border-blue-400 '
+                                    <input className='w-full px-4 py-3 text-gray-800 border rounded-md input input-info '
                                         name='location' id='location' type='text' placeholder='Location' required
                                     />
                                 </div>
                             </div>
                             <div className=' bg-white w-full m-auto rounded-lg'>
                                 <div className='file_upload px-5 py-3 relative border-4 border-dotted border-gray-300 rounded-lg overflow-hidden'>
-                                    <input type='file' name='image' id='image' accept='image/*' className="file-input w-full file-input-secondary focus:border-none " />
+                                    <input type='file' name='image' id='image' accept='image/*' className="file-input w-full file-input-info focus:border-none " />
                                 </div>
                             </div>
                             <div className="flex flex-col md:flex-row lg:flex-row w-full gap-4">
@@ -111,14 +123,36 @@ const AddProducts = () => {
                                     <label htmlFor='location' className='block dark:text-white text-black font-medium'>
                                         Quantity
                                     </label>
-                                    <input className='w-full px-4 py-3 text-gray-800 border rounded-md input border-blue-400 ' name='quantity' id='quantity' type='number' placeholder='Enter quantiey' required
+                                    <input className='w-full px-4 py-3 text-gray-800 border rounded-md input input-info ' name='quantity' id='quantity' type='number' placeholder='Enter quantiey' required
                                     />
                                 </div>
                                 <div className='space-y-1 w-full'>
                                     <label htmlFor='location' className='block dark:text-white text-black font-medium'>
                                         Product Cost
                                     </label>
-                                    <input className='w-full px-4 py-3 text-gray-800 border rounded-md input border-blue-400 ' name='product_cost' id='product_cost' type='number' placeholder='Product Cost' required
+                                    <input className='w-full px-4 py-3 text-gray-800 border rounded-md input input-info ' name='product_cost' id='product_cost' type='number' placeholder='Product Cost' required
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="flex flex-col md:flex-row lg:flex-row w-full gap-4">
+                                <div className='space-y-1 w-full'>
+                                    <label htmlFor='location' className='block dark:text-white text-black font-medium'>
+                                        Category
+                                    </label>
+                                    <select className="select select-info w-full max-w-xs">
+                                        <option disabled selected>Select language</option>
+                                        <option>English</option>
+                                        <option>Japanese</option>
+                                        <option>Italian</option>
+                                    </select>
+
+                                </div>
+                                <div className='space-y-1 w-full'>
+                                    <label htmlFor='location' className='block dark:text-white text-black font-medium'>
+                                        Rating
+                                    </label>
+                                    <input className='w-full px-4 py-3 text-gray-800 border rounded-md input input-info ' name='rating' id='rating' type='number' placeholder='Product Cost' required
                                     />
                                 </div>
                             </div>
@@ -129,7 +163,7 @@ const AddProducts = () => {
                                 <label htmlFor='description' className='block font-medium'>
                                     Description
                                 </label>
-                                <textarea id='description' className='block focus:rose-300 w-full h-32 px-4 py-3 text-gray-800  border rounded-md input border-blue-400  ' name='description' placeholder="Write description"
+                                <textarea id='description' className='block focus:rose-300 w-full h-32 px-4 py-3 text-gray-800  border rounded-md input input-info  ' name='description' placeholder="Write description"
                                 ></textarea>
                             </div>
                             <div className="flex flex-col md:flex-row lg:flex-row w-full gap-4">
@@ -137,14 +171,14 @@ const AddProducts = () => {
                                     <label htmlFor='location' className='block dark:text-white text-black font-medium'>
                                         Product Profit
                                     </label>
-                                    <input className='w-full px-4 py-3 text-gray-800 border rounded-md input border-blue-400 ' name='product_profit' id='product_profit' type='number' placeholder='Product Profit' required
+                                    <input className='w-full px-4 py-3 text-gray-800 border rounded-md input input-info ' name='product_profit' id='product_profit' type='number' placeholder='Product Profit' required
                                     />
                                 </div>
                                 <div className='space-y-1 w-full'>
                                     <label htmlFor='location' className='block dark:text-white text-black font-medium'>
                                         Discount %
                                     </label>
-                                    <input className='w-full px-4 py-3 text-gray-800 border rounded-md input border-blue-400 ' name='discount' id='discount' type='number' placeholder='Discount %' required
+                                    <input className='w-full px-4 py-3 text-gray-800 border rounded-md input input-info ' name='discount' id='discount' type='number' placeholder='Discount %' required
                                     />
                                 </div>
                             </div>
