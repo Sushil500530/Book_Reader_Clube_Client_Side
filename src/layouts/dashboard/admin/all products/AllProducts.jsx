2314763a2bc93ba32b59ca7ec1a6593ed52ede8f
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-20 20:47:56 +0600

    add paginate layout

--- src/layouts/dashboard/admin/all products/AllProducts.jsx
+++ src/layouts/dashboard/admin/all products/AllProducts.jsx
@@ -1,10 +1,8 @@
 import { MdDelete } from "react-icons/md";
 import { FaRegEye } from "react-icons/fa";
 import { BsSearch } from "react-icons/bs";
-import useFurnitures from "../../../../hooks/useFurnitures";
 import { useEffect, useState } from "react";
 const AllProducts = () => {
-    // const [furnitures, refetch, isLoading] = useFurnitures();
     const [furnitures, setFurnitures] = useState([]);
     const [searchFurniture, setSearchFurniture] = useState("");
 
@@ -27,12 +25,11 @@ const AllProducts = () => {
     return (
         <div>
             <div className=" flex items-center justify-between flex-col lg:flex-row container mx-auto">
-               
                     <h1 className="text-3xl text-center font-bold my-5 flex items-center justify-center gap-2 font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#0939e9] to-[#ff0fdb]">All Products({furnitures?.length})</h1>
                 <form className="  h-auto relative flex items-center gap-3">
                     <h1 className="hidden lg:block">Find Product : </h1>
                     <input type="text" name="search" className="rounded-md relative w-auto h-auto" onChange={() => setSearchFurniture(event.target.value)} id="search" placeholder="Search Here....." />
-                    <BsSearch className="absolute top-2 right-2 text-xl text-black" />
+                    <BsSearch className="absolute top-3 right-2 text-xl text-black" />
                 </form>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mt-12">
@@ -55,7 +52,19 @@ const AllProducts = () => {
                         </div>)
                 }
             </div>
-
+            <div className="flex justify-center items-center mb-8">
+                {Array.from({
+                    length: Math.ceil(filteredItems.length / itemsPerPage),
+                }).map((_, index) => (
+                    <button className={`px-3 py-1 mx-1 rounded-full ${currentPage === index + 1 ? "bg-gradient-to-r from-[#0939e8] to-[#ff0fdb] text-white hover:text-black" : "bg-gradient-to-b from-[#2241b0] to-[#000000] text-white"
+                        }`}
+                        key={index + 1}
+                        onClick={() => paginate(index + 1)}
+                    >
+                        {index + 1}
+                    </button>
+                ))}
+            </div>
         </div>
     );
 };
