b39fa8042d199d288d8202768f2c297d4e6453e1
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-21 18:18:00 +0600

    add management product image

--- src/layouts/dashboard/admin/manage products/ManageProductAdmin.jsx
+++ src/layouts/dashboard/admin/manage products/ManageProductAdmin.jsx
@@ -3,6 +3,16 @@ const ManageProductAdmin = () => {
     return (
         <div>
             <h1 className="text-3xl text-center font-bold my-5 flex items-center justify-center gap-2 font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#0939e9] to-[#ff0fdb]">Manage All Products is Comming here</h1>
+            <div>
+                <div className="flex items-center justify-center gap-5 flex-col lg:flex-row">
+                    <div className="w-full lg:w-1/2 h-auto">
+                        <h1>How To Manager My Product</h1>
+                    </div>
+                    <div className="w-full lg:w-1/2 h-[50vh]">
+                        <img src="https://i.ibb.co/1bS7hSz/managment.webp" alt="manage-image" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
