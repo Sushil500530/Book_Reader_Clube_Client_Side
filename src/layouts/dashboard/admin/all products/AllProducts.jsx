8338783905cc5b12647daa340fc6e725a7931bd1
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-28 19:47:27 +0600

    add all funiture data and responsive some  components

--- src/layouts/dashboard/admin/all products/AllProducts.jsx
+++ src/layouts/dashboard/admin/all products/AllProducts.jsx
@@ -70,7 +70,7 @@ const AllProducts = () => {
         <div className="mb-20">
             <SpecificData isOpen={isOpen} closeModal={closeModal} findData={findData} />
             <div className=" flex items-center justify-between flex-col lg:flex-row container mx-auto">
-                <h1 className="text-3xl text-center font-bold my-5 flex items-center justify-center gap-2 font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#0939e9] to-[#ff0fdb]">All Products({furnitures?.length})</h1>
+                <h1 className="text-3xl text-center my-5 flex items-center justify-center gap-2 font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#0939e9] to-[#ff0fdb]">All Products({furnitures?.length})</h1>
                 <form className="  h-auto relative flex items-center gap-3">
                     <h1 className="hidden lg:block">Find Product : </h1>
                     <input type="text" name="search" className="rounded-md relative w-auto h-auto" onChange={() => setSearchFurniture(event.target.value)} id="search" placeholder="Search Here....." />
@@ -80,7 +80,7 @@ const AllProducts = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mt-12">
                 {
                     currentItems?.length > 0 && currentItems?.map(furniture =>
-                        <div key={furniture?._id} className="bg-gradient-to-b from-[#2241b0] to-[#000000] transition ease-in-out text-white transition ease-in-out text-2xl text-center w-full h-auto shadow-2xl relative rounded-md mb-10 ">
+                        <div key={furniture?._id} className="bg-gradient-to-b from-[#2241b0] to-[#000000] text-white transition ease-in-out text-2xl text-center w-full h-auto shadow-2xl relative rounded-md mb-10 ">
                             <div className="flex items-center justify-center w-full py-3 -mt-12">
                                 <figure className="w-32 h-32 ">
                                     <img src={furniture?.image} alt="love-image" className="w-full h-full rounded-full border border-purple-500 cursor-pointer hover:scale-110 transition ease-in-out duration-500" />
--- src/pages/all shop/AllShop.jsx
+++ src/pages/all shop/AllShop.jsx
@@ -2,20 +2,26 @@ import Container from "../../shared/container/Container";
 import { FaSearch } from "react-icons/fa";
 import useFurnitures from './../../hooks/useFurnitures';
 import useFurCategory from "../../hooks/useFurCategory";
+import { useEffect, useState } from "react";
 
 
 const AllShop = () => {
     const [furnitures, refetch, isLoading] = useFurnitures();
+    const [currentFurnitures,setCurrentFurnitures] = useState([]);
     const [category, ,] = useFurCategory();
+console.log(furnitures);
+    // useEffect(()=>{
+    //     const 
+    // },[]);
     return (
         <Container>
             <div>
-                <div className="flex items-center justify-between w-1/2 mx-auto gap-5">
-                    <div className="w-full relative">
+                <div className="flex items-center justify-between flex-col md:flex-row lg:flex-row w-11/12 md:w-2/3 lg:w-1/2 mx-auto gap-5">
+                    <form className="w-full relative">
                         <input type="text" name="search" className="w-full px-4 py-3 text-gray-800 border rounded-md border-blue-400 " placeholder="search your product here....." />
                         <FaSearch className="text-2xl absolute top-3  right-2" />
-                    </div>
-                    <select className="input-bordered w-1/3 px-4 py-3 text-gray-800 border rounded-md border-blue-400 ">
+                    </form>
+                    <select className="input-bordered w-[60%] lg:w-1/3 px-4 py-3 text-gray-800 border rounded-md border-blue-400 ">
                         <option value="filter-category">Filter By Category</option>
                         {
                             category?.length > 0 && category.map(categ => <option key={categ?._id} required>
@@ -23,6 +29,26 @@ const AllShop = () => {
                             </option>)
                         }
                     </select>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mt-12">
+                    {
+                        furnitures?.length > 0 && furnitures?.map(furniture =>
+                            <div key={furniture?._id} className="bg-gradient-to-b from-[#2241b0] to-[#000000] text-white transition ease-in-out text-2xl text-center w-full h-auto shadow-2xl relative rounded-md mb-10 ">
+                                <div className="flex items-center justify-center w-full py-3 -mt-12">
+                                    <figure className="w-32 h-32 ">
+                                        <img src={furniture?.image} alt="love-image" className="w-full h-full rounded-full border border-purple-500 cursor-pointer hover:scale-110 transition ease-in-out duration-500" />
+                                    </figure>
+                                </div>
+                                <h3 className="text-xl">Product Name: {furniture?.title}</h3>
+                                <h3 className="text-xl">Price: $ {furniture?.price}</h3>
+                                <p className='text-[17px] '>Shop Name: {furniture?.shop_name ? furniture?.shop_name : "Anonymous Shop"}</p>
+                                <p className='text-[17px] '>Owner Name: {furniture?.owner_name ? furniture?.owner_name : "Anonymous Owner"}</p>
+                                <div className=" grid grid-cols-2 gap-5 p-5">
+                                    <button className="btn bg-gradient-to-r from-[#0939e8] to-[#ff0fdb] w-full flex items-center justify-center gap-3 border-none outline-none text-base text-white hover:text-blue-600">View Details</button>
+                                    <button className="btn bg-gradient-to-r from-[#0939e8] to-[#ff0fdb] w-full flex items-center justify-center gap-3 border-none outline-none text-base text-white hover:text-red-400">Add to Cart</button>
+                                </div>
+                            </div>)
+                    }
                 </div>
             </div>
         </Container>
