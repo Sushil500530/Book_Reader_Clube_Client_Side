dcd3ae7c1550ed5f7f1eb44829e26fc027974772
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-31 18:04:33 +0600

    add icon in contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,3 +1,7 @@
+import { MdLocationPin,MdLocalPhone ,MdEmail } from "react-icons/md";
+import { BsStopwatch } from "react-icons/bs";
+
+
 
 const Contact = () => {
     return (
@@ -9,27 +13,27 @@ const Contact = () => {
             </div>
             <div className="flex items-center justify-center gap-6 flex-col lg:flex-row-reverse p-5">
                 <div className="w-full h-full lg:w-[35%] space-y-4 text-start flex items-start justify-center flex-col">
-                    <h1 className="font-bold text-transparent text-2xl md:text-5xl lg:text-5xl bg-clip-text bg-gradient-to-r from-[#0939e9] to-[#ff0fdb] text-center">Contact Us</h1>
+                    <h1 className=" my-5 font-bold text-transparent text-2xl md:text-5xl lg:text-5xl bg-clip-text bg-gradient-to-r from-[#0939e9] to-[#ff0fdb] text-center">Contact Us</h1>
                     <div className="flex items-center justify-center gap-6">
-                        <h1>Location</h1>
+                        <h1><MdLocationPin className="p-3 w-12 h-12 bg-white shadow-lg rounded-full" /></h1>
                         <div>
-                            <h1>Stability LTD., Old Town Avenue,</h1>
-                            <small> New York, USA 23000</small>
+                            <h1>Furni-Track LTD,</h1>
+                            <small>Dinajpur, Bangladesh</small>
                         </div>
                     </div>
                     <div className="flex items-center justify-center gap-6">
-                        <h1>Call</h1>
+                        <h1><MdLocalPhone className="p-3 w-12 h-12 bg-white shadow-lg rounded-full" /></h1>
                         <div>
-                            <h1>+1700 124-5678</h1>
-                            <h1>+1700 124-5678</h1>
+                            <h1>+8801767-122497</h1>
+                            <h1>+8801859-011700</h1>
                         </div>
                     </div>
                     <div className="flex items-center justify-center gap-6">
-                        <h1>Message</h1>
+                        <h1><MdEmail className="p-3 w-12 h-12 bg-white shadow-lg rounded-full" /></h1>
                         <h1>sushil500530@gmail.com</h1>
                     </div>
                     <div className="flex items-center justify-center gap-6">
-                        <h1>Time</h1>
+                        <h1><BsStopwatch className="p-3 w-12 h-12 bg-white shadow-lg rounded-full" /></h1>
                         <h1>Monday - Friday 9:00 - 21:00</h1>
                     </div>
                 </div>
