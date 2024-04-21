f9bf36ee3efd133f49e6060c1bf22ef3aa92a281
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-04-21 16:51:27 +0600

    add location icon in google map

--- src/pages/contact/map/SimpleMap.jsx
+++ src/pages/contact/map/SimpleMap.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 import GoogleMapReact from 'google-map-react';
+import { FaLocationDot } from "react-icons/fa6";
 
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({ text:Icon }) => <div className='text-3xl font-bold text-purple-500'>  <span>{<Icon className="w-5 h-8 mr-1 " />}</span></div>;
 
 const SimpleMap = () => {
     const defaultProps = {
@@ -14,16 +14,8 @@ const SimpleMap = () => {
     };
     return (
         <div style={{ height: '60vh', width: '100%' }}>
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: "" }}
-                defaultCenter={defaultProps.center}
-                defaultZoom={defaultProps.zoom}
-            >
-                    <AnyReactComponent
-                        lat={59.955413}
-                        lng={30.337844}
-                        text="Furni-Track"
-                    />
+            <GoogleMapReact  bootstrapURLKeys={{ key: "" }}  defaultCenter={defaultProps.center} defaultZoom={defaultProps.zoom}>
+                    <AnyReactComponent lat={23.8041} lng={90.4152} text={FaLocationDot} />
             </GoogleMapReact>
         </div>
     );
