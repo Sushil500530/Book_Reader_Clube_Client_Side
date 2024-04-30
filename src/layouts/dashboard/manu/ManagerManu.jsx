4530877bf9639113017148e5d1da19093f8601dd
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-04-30 20:05:01 +0600

    add favorite route in manager role from dashboard

--- src/layouts/dashboard/manu/ManagerManu.jsx
+++ src/layouts/dashboard/manu/ManagerManu.jsx
@@ -1,5 +1,5 @@
 import { AiFillInteraction } from "react-icons/ai";
-import { MdAddShoppingCart, MdOutlineManageHistory, MdSummarize , MdOutlinePointOfSale  } from "react-icons/md";
+import { MdAddShoppingCart, MdOutlineManageHistory, MdSummarize , MdOutlinePointOfSale, MdOutlineFavorite  } from "react-icons/md";
 import DManuList from "../../../shared/dashboard manulist/DManuList";
 import { GiBuyCard } from "react-icons/gi";
 const ManagerManu = () => {
@@ -7,6 +7,7 @@ const ManagerManu = () => {
         <>
             <DManuList address={'add-product'} linkTitle={'Add Product'} icon={MdAddShoppingCart} />
             <DManuList address={'my-product'} linkTitle={'My Product'} icon={AiFillInteraction} />
+            <DManuList address={'favorites'} linkTitle={'Favorite Products'} icon={MdOutlineFavorite} />
             <DManuList address={'manage-product'} linkTitle={'Manage Product'} icon={MdOutlineManageHistory } />
             <DManuList address={'sales-product'} linkTitle={'Sale Product'} icon={MdOutlinePointOfSale  } />
             <DManuList address={'sale-summary'} linkTitle={'Sale Summary'} icon={MdSummarize } />
