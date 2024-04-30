de9ccd23e85362b1c697ea86b03446d10e6eeaec
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-04-30 21:16:52 +0600

    add footer page in paymnet section

--- src/pages/checkout/PaymentSection.jsx
+++ src/pages/checkout/PaymentSection.jsx
@@ -4,19 +4,23 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { FaIdCard } from "react-icons/fa";
 import CheckoutForm from "./CheckoutForm";
-
+import FooterPage from './../footer/FooterPage';
 const PaymentSection = () => {
     const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GET_KEY_TOKEN)
     return (
-        <Container>
-            <div className="mt-12">
-                <h1 className="text-2xl font-bold text-center flex items-center justify-center gap-2">Please <FaAmazonPay className="w-14 h-14 text-fuchsia-500" /> Now!</h1>
-                <h1 className="text-xl font-bold text-center flex items-center mt-3 justify-center gap-2">Give Your Card <FaIdCard className="w-7 h-7 text-fuchsia-500" /> Number!</h1>
-                <Elements stripe={stripePromise}>
-                    <CheckoutForm />
-                </Elements>
-            </div>
-        </Container>
+
+        <>
+            <Container>
+                <div className="mt-12">
+                    <h1 className="text-2xl font-bold text-center flex items-center justify-center gap-2">Please <FaAmazonPay className="w-14 h-14 text-fuchsia-500" /> Now!</h1>
+                    <h1 className="text-xl font-bold text-center flex items-center mt-3 justify-center gap-2">Give Your Card <FaIdCard className="w-7 h-7 text-fuchsia-500" /> Number!</h1>
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
+                </div>
+            </Container>
+            <FooterPage />
+        </>
     );
 };
 
