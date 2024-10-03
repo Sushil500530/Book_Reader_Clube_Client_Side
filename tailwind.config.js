e9b0d1fe20765e66914a97ac8dfa298903426b70
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-10-04 00:01:51 +0600

    add change google font and customize primary color wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'custom-gradient': 'linear-gradient(82.84deg, #2241b0 0.86%, #000000 100%)',
+    },
   },
   plugins: [require("daisyui"), require('@tailwindcss/forms'),],
 }
