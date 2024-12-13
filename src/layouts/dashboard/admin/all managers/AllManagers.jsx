bf75b6f4b3c144b72fe9bd0d93b8a37494b9cbba
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-12-13 22:29:52 +0600

    also customize all manager section

--- src/layouts/dashboard/admin/all managers/AllManagers.jsx
+++ src/layouts/dashboard/admin/all managers/AllManagers.jsx
@@ -43,7 +43,7 @@ const AllManagers = () => {
     if (isLoading) {
         return <Loader />
     }
-// console.log(managers);
+    // console.log(managers);
     return (
         <div>
             <ManagerModal isOpen={isOpen} setIsOpen={setIsOpen}>
@@ -53,11 +53,11 @@ const AllManagers = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-12 lg:gap-5 mt-12">
                 {
                     managers?.map(item =>
-                        <div key={item._id} className="bg-color transition ease-in-out text-white text-2xl text-center w-full h-auto bg-white shadow-2xl relative rounded-md ">
+                        <div key={item._id} className="bg-color transition ease-in-out text-white text-2xl text-center w-full h-auto bg-white shadow-2xl relative rounded-md mb-10">
                             <div className="flex items-center justify-center w-full py-3 -mt-12">
                                 <figure className="w-24 h-24 ">
                                     <img src={item?.shop_logo} alt="love-image" className="w-full h-full rounded-full border border-purple-500" />
-                                </figure>   
+                                </figure>
                             </div>
                             <h3 className="text-xl"><span className="font-bold">{item?.owner}</span></h3>
                             <p className='text-[17px] '>Shop: <span className="font-bold">{item?.shop_name}</span></p>
