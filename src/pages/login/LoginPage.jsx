276db912781c89d078c09df287735e53a75928fe
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-12-13 21:56:32 +0600

    add login for admin , manager also guest in login page

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,12 +1,12 @@
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import loginImage from '../../assets/image/authentication/undraw_fingerprint_login_re_t71l.svg'
+import loginImage from '../../assets/image/authentication/undraw_fingerprint_login_re_t71l.svg';
 import Container from '../../shared/container/Container';
 import SocialAccount from '../../shared/socialAccount/SocialAccount';
 import { useAuth } from '../../hooks/useAuth';
 import toast from 'react-hot-toast';
 import useAxiosPublic from '../../hooks/useAxiosPublic';
 import { FaEyeSlash, FaRegEye, FaSpinner } from 'react-icons/fa';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const LoginPage = () => {
     const axiosPbulic = useAxiosPublic();
@@ -15,33 +15,33 @@ const LoginPage = () => {
     const [showPassword, setShowPassword] = useState(false);
     const navigate = useNavigate();
     const location = useLocation();
+    const emailRef = useRef(null); // Ref for email input
+    const passwordRef = useRef(null); // Ref for password input
+
     const handleSubmit = async (e) => {
         setLoading(true);
         e.preventDefault();
         const form = e.target;
         const email = form.email.value;
         const password = form.password.value;
-        // console.log('button clieked', email, password);
+
         await loginUser(email, password)
             .then(res => {
                 if (res.user) {
-                    navigate(location?.state ? location?.state : '/')
+                    navigate(location?.state ? location?.state : '/');
                     toast.success('Logged in successfully');
-                    return setLoading(false)
+                    return setLoading(false);
                 }
-
             })
             .catch(error => {
-                setLoading(false)
-                toast.error(error.message)
-            }
-            )
-
-    }
+                setLoading(false);
+                toast.error(error.message);
+            });
+    };
 
     const handleShowPassword = () => {
-        setShowPassword(!showPassword)
-    }
+        setShowPassword(!showPassword);
+    };
 
     const handleGoogleSignIn = () => {
         setLoading(true);
@@ -56,56 +56,106 @@ const LoginPage = () => {
                 };
                 axiosPbulic.post('/users', userInfo)
                     .then(() => { })
-                    .catch(error =>{
+                    .catch(error => {
                         setLoading(false);
-                        toast.error(error.message)
-                    })
-                // console.log(location?.state);
+                        toast.error(error.message);
+                    });
                 navigate(location?.state ? location?.state : '/');
                 return toast.success('Login Successfull...!');
-
             })
             .catch(error => {
-                setLoading(false)
-                toast.error(error.message)
-            })
+                setLoading(false);
+                toast.error(error.message);
+            });
+    };
 
-    }
+    const handleAdminLogin = () => {
+        if (emailRef.current && passwordRef.current) {
+            emailRef.current.value = 'admin@demo.com';
+            passwordRef.current.value = '1122AAaa@@';
+        }
+    };
+    const handleManagerLogin = () => {
+        if (emailRef.current && passwordRef.current) {
+            emailRef.current.value = 'manager@demo.com';
+            passwordRef.current.value = '1122AAaa@@';
+        }
+    };
+    const handleGuestLogin = () => {
+        if (emailRef.current && passwordRef.current) {
+            emailRef.current.value = 'guest@demo.com';
+            passwordRef.current.value = '1122AAaa@@';
+        }
+    };
 
     return (
         <div className="bg-[url('https://i.postimg.cc/RFDMMbwn/bg-image1.jpg')] w-full min-h-screen bg-cover bg-no-repeat ">
-            <Container> 
+            <Container>
                 <div className="container mx-auto flex flex-col lg:flex-row items-center justify-center gap-5 mb-12">
                     <div className="w-full min-h-[70vh] lg:w-3/5 flex items-center justify-center p-5 rounded-md">
                         <img src={loginImage} alt="login-image" />
                     </div>
                     <div className="card-body p-0 lg:p-8 m-5 w-full lg:w-1/2 min-h-[70vh] text-white bg-gray-700/50 backdrop-blur-md">
                         <form onSubmit={handleSubmit} className="p-5 space-y-3">
-                            <h1 className="text-3xl  font-bold mb-12">Please Login🤔</h1>
+                            <h1 className="text-3xl font-bold mb-12">Please Login🤔</h1>
                             <div className="space-y-3">
-                                <label className="text-[18px]  font-medium">Email Address</label>
-                                <input type="email" name="email" className="bg-transparent px-4 py-3 text-gray-200 w-full border rounded-md border-blue-400  mb-1" id="" placeholder="Enter your username or address..." required />
+                                <label className="text-[18px] font-medium">Email Address</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    ref={emailRef} // Attach ref to email input
+                                    className="bg-transparent px-4 py-3 text-gray-200 w-full border rounded-md border-blue-400 mb-1"
+                                    placeholder="Enter your username or address..."
+                                    required
+                                />
                             </div>
                             <div className="space-y-3 relative">
                                 <label className="text-[18px] font-medium">Password</label>
-                                <input type={`${showPassword ? "text" : "password"}`} name="password" className=" bg-transparent px-4 py-3 text-gray-200 w-full border rounded-md border-blue-400 " id="" placeholder="Enter your password..." required />
-                                <span onClick={handleShowPassword} className='absolute top-10 right-2 cursor-pointer'>{
-                                    showPassword ?
-                                        <FaRegEye className='text-2xl text-white' />
-                                        :
-                                        <FaEyeSlash className='text-2xl text-white' />
-                                }</span>
+                                <input
+                                    type={`${showPassword ? 'text' : 'password'}`}
+                                    name="password"
+                                    ref={passwordRef} // Attach ref to password input
+                                    className="bg-transparent px-4 py-3 text-gray-200 w-full border rounded-md border-blue-400"
+                                    placeholder="Enter your password..."
+                                    required
+                                />
+                                <span onClick={handleShowPassword} className="absolute top-10 right-2 cursor-pointer">
+                                    {showPassword ? (
+                                        <FaRegEye className="text-2xl text-white" />
+                                    ) : (
+                                        <FaEyeSlash className="text-2xl text-white" />
+                                    )}
+                                </span>
                             </div>
-                            <p className="text-base font-medium my-8">Don&apos;t have an account yet?{' '} Please <Link to='/signup' className="text-blue-500 underline text-lg">Sign up</Link></p>
-                            <button type='submit' className="btn px-8 text-white bg-gradient-to-r from-[#0939e8] to-[#ff0fdb] text-[18px] outline-none border-none font-medium hover:text-blue-500 w-full">
+                            <p className="text-base font-medium my-8">
+                                Don&apos;t have an account yet?{' '}
+                                Please{' '}
+                                <Link to="/signup" className="text-blue-500 underline text-lg">
+                                    Sign up
+                                </Link>
+                            </p>
+                            <button
+                                type="submit"
+                                className="btn px-8 text-white bg-gradient-to-r from-[#0939e8] to-[#ff0fdb] text-[18px] outline-none border-none font-medium hover:text-blue-500 w-full"
+                            >
                                 {loading ? (
-                                    <span className='flex items-center justify-center gap-3'> <FaSpinner className='m-auto animate-spin' size={24} /> Processing....</span>
+                                    <span className="flex items-center justify-center gap-3">
+                                        <FaSpinner className="m-auto animate-spin" size={24} /> Processing....
+                                    </span>
                                 ) : (
                                     'Sign in'
                                 )}
-
                             </button>
                         </form>
+
+                        <div className="flex items-center justify-center gap-5">
+                            <button className="btn btn-info"  onClick={handleAdminLogin}>Admin Login</button>
+                            <button className="btn btn-info"  onClick={handleManagerLogin}>Manager Login</button>
+                            <button className="btn btn-info" onClick={handleGuestLogin}>
+                                Guest Login
+                            </button>
+                        </div>
+
                         <div className="w-[90%] md:w-2/3 lg:w-[80%] mx-auto pb-6">
                             <div className="divider divide-x-0 divider-primary text-2xl">Or</div>
                             <div className="space-y-3 mt-6 w-full">
@@ -114,7 +164,7 @@ const LoginPage = () => {
                         </div>
                     </div>
                 </div>
-            </Container >
+            </Container>
         </div>
     );
 };
