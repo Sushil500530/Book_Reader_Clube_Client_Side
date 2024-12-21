cb508a5b95939c152d39aa4a3f8693aca5fa06eb
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-12-21 13:28:25 +0600

    add loader functionlity

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,26 @@
 import Header from "../components/header/Header";
+import { useState, useEffect } from 'react';
 
 const MainLayout = () => {
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
+        const stackTime = () => {
+            setTimeout(() => {
+                setIsLoading(false)
+            }, 3000);
+        }
+        stackTime()
+    }, [])
     return (
+
         <div>
-            <Header />
+            {
+                isLoading ?
+                    <div className='w-full h-screen flex items-center justify-center'>
+                        <h2 className='text-5xl font-bold text-center'>Loading.....</h2>
+                    </div> :
+                    <Header />
+            }
         </div>
     );
 };
