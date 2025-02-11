efeae6afb965adbe8d0505fadad2280cd8d43563
Sushil Hemrom <sushilhemrom98@gmail.com>
2025-02-11 23:55:38 +0600

    add pie chart

--- src/layouts/dashboard/admin/admin-home/pie-chart/index.jsx
+++ src/layouts/dashboard/admin/admin-home/pie-chart/index.jsx
@@ -1,6 +1,74 @@
+import Chart from "react-apexcharts";
 
-export default function PieChart() {
+export function PieChart() {
+    const options = {
+        chart: {
+            width: 380,
+            type: "donut",
+        },
+        plotOptions: {
+            pie: {
+                startAngle: -90,
+                endAngle: 270,
+                donut: {
+                    labels: {
+                        show: true,
+                        total: {
+                            show: true,
+                            label: 'Total Clicks',
+                            color: '#373d3f',
+                            formatter: (w) => {
+                                return w.globals.seriesTotals.reduce((a, b) => {
+                                    return a + b;
+                                }, 0);
+                            },
+                        },
+                    },
+                },
+            },
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        fill: {
+            type: "gradient",
+        },
+        legend: {
+            position: "bottom",
+            formatter: (val, opts) => {
+                return `${val} - ${opts.w.globals.series[opts.seriesIndex]}`;
+            },
+        },
+        title: {
+            text: "Clicks Per Devices",
+            align: "center",
+            style: {
+                fontSize: "16px",
+                color: "#373d3f",
+            },
+        },
+        labels: ["Android", "iOS", "Desktop"],
+        colors: ["#008FFB", "#00E396", "#FEB019"],
+        responsive: [
+            {
+                breakpoint: 480,
+                options: {
+                    chart: {
+                        width: 200,
+                    },
+                    legend: {
+                        position: "bottom",
+                    },
+                },
+            },
+        ],
+    };
+
+    const series = [44, 55, 41]; // Example data for Android, iOS, and Desktop
+
     return (
-        <div>PieChart</div>
-    )
+        <div id="chart">
+            <Chart options={options} series={series} type="donut" width="380" />
+        </div>
+    );
 }
